660ef6b946b499f56d93144ba4136284acba6ffa
Ashik <ashikofficial455@gmail.com>
2024-01-06 23:00:35 +0530

    idk

--- prototype.py
+++ prototype.py
@@ -1,14 +1,26 @@
 import json
-data_holder = []
-while True:    
-    todo = input("What would you like to do? ")
-    if todo == "quit":
-        break
-    else:
-        data_master = {"todo": todo,"done": False}
-    with open("data.json", "a") as fobj:
-        json.dump(data_master, fobj)
 
-with open("data.json", "r") as fobj:
-    json_data = json.load(fobj)
-    print(json_data)
+data_holder = []
+
+def saveTodo():
+    while True:    
+        todo = input("What would you like to do? ")
+        if todo == "quit":
+            break
+        else:
+            data_master = {"todo": todo, "done": False}
+            data_holder.append(data_master)  
+
+    with open("data.json", "a") as fobj:
+        for data in data_holder:
+            json.dump(data, fobj)
+            fobj.write('\n')
+
+def show():
+    with open("data.json", "r") as fobj:
+        json_data = [json.loads(line) for line in fobj]
+        print(json_data)
+        
+
+saveTodo()
+show()
