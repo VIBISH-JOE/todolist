fe87568d6a3acdf43c3e4693cafe8d35bdb168fc
Ashik <ashikofficial455@gmail.com>
2024-01-06 22:29:54 +0530

    initial commit

---
+++ prototype.py
@@ -0,0 +1,15 @@
+import json
+data_holder = []
+while True:    
+    todo = input("What would you like to do? ")
+    if todo == "quit":
+        break
+    else:
+        data_master = {"todo": todo,"done": False}
+    with open("data.json", "a") as fobj:
+        json.dump(data_master, fobj)
+
+with open("data.json", "r") as fobj:
+    json_data = json.load(fobj)
+    print(json_data)
+    
