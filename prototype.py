c285f0845c46044fdedb814e3107df94ea35f1cb
Ashik <ashikofficial455@gmail.com>
2024-01-07 09:28:47 +0530

    fixed bugs

--- prototype.py
+++ prototype.py
@@ -2,28 +2,28 @@ import json
 
 data_holder = []
 
+try:
+    with open('data.json', 'r') as fobj:
+        data_holder = json.load(fobj)
+        if len(data_holder) == 0:
+             print("empty")
+except FileNotFoundError:
+    print("No data found.")
+
 def saveJson():
     while True:
         todo = input("Enter the todo list (type 'quit' to exit): ")
         if todo.lower() == 'quit':
+            print(data_holder)
             break
         else:
             data = {
-                "todo": todo
+                "todo": todo,
+                "done": False
             }
             data_holder.append(data)
             with open("data.json", 'w') as jfobj:
-                json.dump(data_holder, jfobj)
+                json.dump(data_holder, jfobj, indent=2)
 
-def showJson():
-    try:
-        with open('data.json', 'r') as fobj:
-            data = json.load(fobj)
-            print(data)
-    except FileNotFoundError:
-
-        print("No data found.")
-        
 # Main
 saveJson()
-showJson()
