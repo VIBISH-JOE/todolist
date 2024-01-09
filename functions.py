b7947396d1105fd7b35ae71b74f5c3ee217b4207
Ashik <ashikofficial455@gmail.com>
2024-01-09 22:41:36 +0530

    fixed json turning true

--- functions.py
+++ functions.py
@@ -24,15 +24,21 @@ def write():
         with open('data.json','w') as fobj:
             json.dump(data_holder,fobj,indent=2)
         print("done")
+        
 
 def marktodo(mktodo):
+    read()
     todo_found = False
+    print(data_holder)
     for i in data_holder:
         keylst = list(i.values())
+        print(keylst)
         if mktodo in keylst:
             todo_found = True
             break
     if todo_found:
-        return 'yes'
+        keylst[1] = True
+        print(keylst)
+        return 'Found'
     else:
         return 'nah'
--- prototype.py
+++ prototype.py
@@ -1,6 +1,5 @@
 import functions as fn
 
-fn.read()
 # fn.write()
 
 mktodo = input("Enter the Todo That you want to mark as done")
